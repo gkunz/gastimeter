dd262b00bb23dc44fe785c5bee87fce8f8a9b5d4
Georg Kunz <der.schorsch@gmail.com>
2024-01-16 22:47:01 +0100

    Adding basic unit tests and corresponding GitHub action

---
+++ tests/unit_tests.py
@@ -0,0 +1,39 @@
+import cv2 as cv
+import json
+import os
+import unittest
+
+from gastimeter.responseparser import parse_response
+from gastimeter.preprocessor import preprocess_image
+
+class TestResponseParser(unittest.TestCase):
+    '''
+    Basic unit tests. 
+    '''
+
+    def test_pre_process_image(self):
+        '''
+        Test the image pre-processor. There is no assertion, but instead if all
+        operations success, the test is assumed to pass.
+        '''
+        filename = os.path.join('testdata', 'test-image.jpg')
+        if not os.path.exists(filename):
+            self.assertTrue(False, f'Image \'{filename}\' not found.')
+
+        image = cv.imread(filename)
+        image = preprocess_image(image)
+
+
+    def test_parsing_static_response(self):
+        '''
+        Test the response parser on a static reponse json.
+        '''
+        with open(os.path.join('testdata', 'test-response.json')) as f:
+            content = f.read()
+            response = json.loads(content)
+            reading = parse_response(response)
+            self.assertEqual(reading, 4663.51)
+
+
+if __name__ == '__main__':
+    unittest.main()
